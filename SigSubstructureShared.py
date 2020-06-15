1306a387620d7fb555d711b76325139a33fbae25
arwhirang <limsangrak@korea.ac.kr>
2020-06-15 17:47:52 +0200

    Update SigSubstructureShared.py

--- SigSubstructureShared.py
+++ SigSubstructureShared.py
@@ -20,6 +20,8 @@ def sigIdxAtom(sigIdx, currSmiList):
     atomcnt = -1#first atom is 0
     for idx, ele in enumerate(currSmiList):
         if ele in twoChars or ele.isalpha():# or ele.isdigit():
+            if ele == "@" or ele == "H":#these are the same as the bond signals
+                continue
             atomcnt += 1
             if idx in sigIdx:
                 sigIdx_general.append(idx)
@@ -28,134 +30,112 @@ def sigIdxAtom(sigIdx, currSmiList):
 
 def get_substruct(mol, atom_idx_list, radius = 3):
     # this function creates submolecules
-    smiList = {}
-    for r in range(radius)[::-1]:
+    smiDic = {}
+    for r in range(2, radius)[::-1]:
         #can extract the submolecule consisting of all atoms within a radius of r of atom_idx
         for atom_idx in atom_idx_list:
-            print("atom_idx",atom_idx)
+            #print("atom_idx",atom_idx)
             env = Chem.FindAtomEnvironmentOfRadiusN(mol, r, atom_idx)
-            amap = {}
+            amap = {}#key, val = <significant atom index(different from whole index, order)>
             submol = Chem.PathToSubmol(mol, env, atomMap=amap)
-            smi = Chem.MolToSmiles(submol)
+            subsmi = Chem.MolToSmiles(submol)
 
-            if smi != "":
+            if subsmi != "":
                 #found the submolecule
                 #break
-                smiList[smi] = 1
-    return list(smiList)
+                smiDic[subsmi] = amap.keys()
+    return smiDic
 
-#due to the nature of our extracted substructure, we need to fill the hole
-#if the atoms are 1 distance away (an atom is not included in the list) => fill the gap
-#if the atoms are 2 distance away => discard the atom outlier
-def idxMergeAtomLists(sigIdx_general, sigIdx_atom):
-    if len(sigIdx_general) == 1:
-        return sigIdx_general
-    retlist = []
-    twoChars = {"AL":1, "al":1, "Al":1, "AU":1, "au":1, "Au":1, "MG":1, "mg":1, "Mg":1, "ZN":1, "zn":1, "Zn":1, "CA":1, "ca":1, "Ca":1, "NA":1, "na":1, "Na":1, "CL":1, "cl":1, "Cl":1, "FE":1, "fe":1, "Fe":1, "BR":1, "br":1, "Br":1, "SI":1, "si":1, "Si":1}
-    for idx, ele in enumerate(sigIdx_general):
-        if idx == len(sigIdx_general) - 1:
-            break
-        if sigIdx_atom[idx + 1] - sigIdx_atom[idx] > 2:
-            continue
-        else:#if sigIdx_atom[idx + 1] - sigIdx_atom[idx] == 1 or sigIdx_atom[idx + 1] - sigIdx_atom[idx] == 2:
-            if ele not in retlist:#becuase we add the next element ...
-                retlist.append(ele)
-            tmplist = []
-            for i in range(sigIdx_general[idx] + 1, sigIdx_general[idx + 1]):
-                tmplist.append(i)
-            retlist.extend(tmplist)
-            retlist.append(sigIdx_general[idx + 1])
-    return retlist
+# this function updates dicSMA
+#key, value: <substr in str, list of scores> / each of the scores are the sum of the atoms in the smart
+def get_scores(substr_dic, currsmi, layer_weights, dicSMA):
+    for substr in substr_dic.keys():
+        start = get_start(substr, currsmi)
+        if start == -1:
+            print("error!", substr, currsmi)
+        score = 0.0
+        for i in range(start, start + len(substr)):
+            score += layer_weights[i]
+        if substr in dicSMA:
+            dicSMA[substr].append(score)
+        else:
+            dicSMA[substr] = [score]
 
-#related to idxMergeAtomLists
-#this func connects the original SMILES with the significant atoms
-def atomListsIntoSmiFrags(mergedAtomList, currSmiList):
-    retlist = []
-    prevIndex = 0
-    tmplist = []
-    for i, ele in enumerate(mergedAtomList):
-        if i != 0:
-            if ele - prevIndex > 1:
-                retlist.append("".join(tmplist))
-                tmplist = []
-        tmplist.append(currSmiList[ele])
-        prevIndex = ele
-        
-    if tmplist != []:
-        retlist.append("".join(tmplist))
-    return retlist
+IGweights, teststr = pickle.load(open( "save_igtest2", "rb" ))
+bioalert_subst = pickle.load(open( "nr-ar_bioalerts", "rb" ))
 
-attnweights, teststr = pickle.load(open( "save_igtest2", "rb" ))
-bioalert_nr_ar_lbd = pickle.load(open( "nr-ar_bioalerts", "rb" ))
-
-dic = {}
+###########################
+#this part is for checking only
+dic0 = {}
 act1, act0 = 0, 0
-for idx, key in enumerate(bioalert_nr_ar_lbd['Substructure in Molecule']):#molecule duplicated
-    actlabel = bioalert_nr_ar_lbd['Activity label'][idx]
+for idx, key in enumerate(bioalert_subst['Substructure in Molecule']):#molecule duplicated
+    actlabel = bioalert_subst['Activity label'][idx]
     if actlabel == 1.0:
         act1 += 1
-        dic[Chem.MolToSmiles(key)] = 1
+        dic0[Chem.MolToSmiles(key)] = 1
     else:
         act0 += 1
-        if Chem.MolToSmiles(key) in dic and dic[Chem.MolToSmiles(key)] == 1:
-            dic[Chem.MolToSmiles(key)] = 1
+        if Chem.MolToSmiles(key) in dic0 and dic0[Chem.MolToSmiles(key)] == 1:
+            dic0[Chem.MolToSmiles(key)] = 1
         else:
-            dic[Chem.MolToSmiles(key)] = 0
-print(act1, act0)
-print(len(dic))
-
-cnt, cntPos = 0, 0
-for currSmiList in teststr:
-    currsmi = "".join(currSmiList)
-    if currsmi in dic:
-        cnt += 1
-        if dic[currsmi] == 1:
-            cntPos += 1
-print(cnt, cntPos)
-
-dic = {}
-for idx, key in enumerate(bioalert_nr_ar_lbd['Substructure in Molecule']):#molecule duplicated
-    smikey = Chem.MolToSmiles(key)
-    if smikey in dic:
-        dic[smikey].append(idx)
-    else:
-        dic[smikey] = [idx]
-print(len(dic))
-
-def howManyAshareB(A, B):
-    share, notshare = 0, 0
-    for ele in A:
-        if ele in B:
-            share += 1
-        else:
-            notshare += 1
-    return share, notshare, share + notshare
+            dic0[Chem.MolToSmiles(key)] = 0
+print("", act1, act0)
+print(len(dic0))
+############################
 
 
-layer1_max = tf.math.reduce_max(attnweights, axis=2)#layer1_max shape = (whole_size, seq_len)
+
+#make dicSMA
+#key, value: <substr in str, list of scores> / each of the scores are the sum of the atoms in the smart
+dicSMA = {}
+layer1_sum = tf.math.reduce_sum(IGweights, axis=2)#layer1_max shape = (whole_size, seq_len)
 shared, notShared = 0, 0
 for i, currSmiList in enumerate(teststr):
     currsmi = "".join(currSmiList)
-    print("currsmi:",currsmi)
     currmol = Chem.MolFromSmiles(currsmi)
-    if currsmi in dic:
-        th_Until7thW = sorted(layer1_max[i])[-7]
-        sigIdxList = idxForSignificants(th_Until7thW, layer1_max[i])
-        sigIdx_general, sigIdx_atom = sigIdxAtom(sigIdxList, teststr[i])
-        smarts_list = get_substruct(currmol, sigIdx_atom)        
-        #sigIdx_atom_merged = idxMergeAtomLists(sigIdx_general, sigIdx_atom)
-        #smarts_list = atomListsIntoSmiFrags(sigIdx_atom_merged, teststr[i])
-        isSmartsIn = False
-        for idx in dic[currsmi]:
-            sigAtomsInMol = list(currmol.GetSubstructMatch(bioalert_nr_ar_lbd['Substructure'][idx]))
-            substructure_smi = Chem.MolToSmiles(bioalert_nr_ar_lbd['Substructure'][idx])
-            #print(sigAtomsInMol, Chem.MolToSmiles(bioalert_nr_ar_lbd['Substructure'][idx]))
-            for smart in smarts_list:
-                if smart.lower() in substructure_smi.lower():
-                    isSmartsIn = True
-                    print(smart, substructure_smi)
-        if isSmartsIn:
-            shared += 1
-        else:
-            notShared += 1
+
+    th_Until7thW = sorted(layer1_sum[i])[-7]
+    sigIdxList = idxForSignificants(th_Until7thW, layer1_sum[i])
+    sigIdx_general, sigIdx_atom = sigIdxAtom(sigIdxList, teststr[i])
+    substrDic = get_substruct(currmol, sigIdx_atom)#substrs of the current smi
+    get_scores(substr_list, currsmi, layer1_sum[i], dicSMA)
+
+#calculate global mean score of every substrs
+gmSMA = {}
+for key, val in dicSMA.items():
+    gmSMA[key] = mean(val)
+    print(gmSMA[key])
+#extract importnat structures
+
+#compare
+
+"""
+
+dic_bioalert = {}
+for idx, key in enumerate(bioalert_subst["Substructure"]):
+    actlabel = bioalert_subst['Activity label'][idx]
+    subsmi = Chem.MolToSmiles(key)
+    if subsmi in dic_bioalert:
+        dic_bioalert[subsmi].append(idx)
+    else:
+        dic_bioalert[subsmi] = [idx]
+print(len(dic_bioalert))
+
+
+    isSmartsIn = False
+    for idx in dic[currsmi]:
+        sigAtomsInMol = list(currmol.GetSubstructMatch(bioalert_subst['Substructure'][idx]))
+        substructure_smi = Chem.MolToSmiles(bioalert_subst['Substructure'][idx])
+        #print(sigAtomsInMol, Chem.MolToSmiles(bioalert_nr_ar_lbd['Substructure'][idx]))
+        for smart in smarts_list:
+            if smart.lower() in substructure_smi.lower():
+                isSmartsIn = True
+                print("currsmi:",currsmi)
+                print(smart, substructure_smi)
+    if isSmartsIn:
+        shared += 1
+    else:
+        notShared += 1
 print(shared, notShared)
+"""
+
